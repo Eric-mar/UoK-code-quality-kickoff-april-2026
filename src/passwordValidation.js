712a4bcbd3e138176fff6e27744c995dfc899c27
tuyishime eric <erictuyishime2004@gmail.com>
2026-04-17 09:12:00 +0200

    changing the response to boolean

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -7,40 +7,34 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @returns {boolean}
  */
 let password = ""
-let removingLetters = password.replace(/[A-Za-z]/g,'').split('').sort((a,b)=>b-a)
-let sortingPassword=removingLetters
+let removingLetters = password.replace(/[A-Za-z]/g, '').split('').sort((a, b) => b - a)
+let sortingPassword = removingLetters
 export default function isValidPassword(password) {
   if (typeof password !== "string") password = String(password);
-   const validPassword = /^[A-Za-z0-9]+$/.test(password)
-  if(password.length !== 10){
-    return 'the password should be atleast 10 characters'
-  }
-  else if(!validPassword){
-    return "the inserted character is wrong"
-  }
-  // containing letters
-  else if (!/^[A-Za-z]/.test(password)) {
-  return "the password should contain with a letter";
-}
-// password should contain numbers
-else if(!/[0-9]/.test(password)){
-    return "password should have numbers"
-  }
-//password should conatin upperCase
-
-else if(!/[A-Z]/.test(password)){
-    return "the password should have uppercase value" 
-  } 
-  // password should ot be sorted
-  else if(sortingPassword){
-    return "password should not be ascending or descending order"
-  }
-//password should have lowerCase
-else if(!/[a-z]/.test(password)){
-    return "the password should have at least lowerCase"
-  }
-
+  const validPassword = /^[A-Za-z0-9]+$/.test(password)
+  if (password.length !== 10) return false
   
+ if (!validPassword) return false
+  
+  // containing letters
+   if (!/^[A-Za-z]/.test(password)) return false
+  
+  // password should contain numbers
+   if (!/[0-9]/.test(password)) return false
+  
+  //password should conatin upperCase
+
+   if (!/[A-Z]/.test(password)) return false
+  
+  // password should ot be sorted
+   if (sortingPassword) 
+    return false
+  
+  //password should have lowerCase
+   if (!/[a-z]/.test(password)) return false
+  
+
+
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
   return true;
